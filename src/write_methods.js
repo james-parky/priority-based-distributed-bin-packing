bbd1e7a0c837d909832decfe9e6d3d6c553dc774
jame-parky <72101291+james-parky@users.noreply.github.com>
2022-03-22 00:56:22 +0000

    Put write methods into seperate file and commented methods using JSDocs format

---
+++ src/write_methods.js
@@ -0,0 +1,54 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const fs = require("fs");
+
+/**
+ * Outputs the given object as a .json file with the given file name
+ * @method
+ * @param {object} matches JavaScript Object Notation (JSON) object of matches made between supervisors and students
+ * @param {string} fileName filename
+ * @throws {NodeJS.ErrnoException}
+ */
+export const writeToJson = (matches, fileName) => {
+    var json = JSON.stringify(matches);
+    fs.writeFile(fileName, json, "utf8", (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+};
+
+/**
+ * Formats matches made between supervisors and students into a more human-readable format
+ * @method
+ * @param {object} unformattedMatches JavaScript Object Notation (JSON) object of matches made between supervisors and students
+ * @returns {object} a human-readble version of the unformattedMatches paramter
+ */
+const formatMatches = (unformattedMatches) => {
+    var formattedMatches = [];
+    unformattedMatches.finalMatches.forEach((supervisor) => {
+        var matches = [];
+        supervisor.matchedStudentResponses.forEach((student) => {
+            matches.push({
+                studentResponseId: student.studentResponse.responseId,
+                commonACMKeywordId: student.commonACMKeywordId,
+            });
+        });
+        formattedMatches.push({
+            supervisorResponseId: supervisor.supervisorResponse.responseId,
+            capacity: supervisor.supervisorResponse.capacity,
+            matches,
+        });
+    });
+    return formattedMatches;
+};
+
+/**
+ * Formats matches made between supervisors and students into a more human-readable format and outputs the result to a .json file with the given file Name
+ * @method
+ * @param {object} unformattedMatches JavaScript Object Notation (JSON) object of matches made between supervisors and students
+ * @param {string} fileName filename
+ */
+export const formatAndWriteToJson = (unformattedMatches, fileName) => {
+    writeToJson(formatMatches(unformattedMatches), fileName);
+};
