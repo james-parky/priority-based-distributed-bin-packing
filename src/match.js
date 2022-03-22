5d1f419da59bfa642f779cffaf2788457b7928d7
jame-parky <72101291+james-parky@users.noreply.github.com>
2022-03-22 19:45:41 +0000

    Removed unused code

--- src/match.js
+++ src/match.js
@@ -110,34 +110,6 @@ const findMatches = (parsedStudentResponses, parsedSupervisorResponses) => {
     return { matches: allMatches, unmatchedStudents };
 };
 
-/**
- * Rematches any students that oversubscribed a supevisor with supervisors that
- * had space remaining. Students are orderd on a first come first serve basis
- * Students that still dont get matched to a supervisor are categorised and returned
- * as unmatched
- * @func
- * @param {object[]} students an array of parsed student responses
- * @param {object[]} supervisors an array of parsd supervisor responses
- * @returns {object} {the matches made between the initially unmatched students and supervisors with space remaining, unmatched students}
- */
-const matchRemainders = (students, supervisors) => {
-    var totalRemainingSpaces = supervisors
-        .map((supervisor) => supervisor.capacity)
-        .reduce((prev, current) => prev + current, 0);
-
-    // Only keep enough students for the remaining spaces
-    if (students.length > totalRemainingSpaces) {
-        students = students.slice(0, totalRemainingSpaces);
-        var unmatched = students.slice(totalRemainingSpaces);
-    }
-
-    var matchesFromRemainders = findMatches(students, supervisors);
-    console.log(matchesFromRemainders);
-    writeToJson({ matchesFromRemainders }, "json/newMatches.json");
-    console.log(unmatched);
-    return { matchesFromRemainders, unmatched };
-};
-
 const orderMatches = (matches) => {
     matches.forEach((supervisor) => {
         supervisor.matchedStudentResponses.sort((student1, student2) =>
@@ -193,111 +165,30 @@ const reviewMatches = async (matches, studentsPerSupervisor, remainder) => {
         );
     });
     return { matches, supervisorsWithNoMatches };
-    // // Find all possible matches and those students left over with no matches
-    // var { matches, unmatchedStudents } = findMatches(
-    //     await getParsedStudentResponses(),
-    //     await getParsedSupervisorResponses()
-    // );
-    // writeToJson({ matches }, "json/matches.json");
-    // writeToJson({ unmatchedStudents }, "json/unmatchedStudents.json");
-    // var leftOverStudents = unmatchedStudents;
-    // var rejectedStudents = [];
-    // var supervisorsWithLeftOverSpace = [];
-    // // 52 across for 12 supervisors
-    // // 7 students
-    // var totalCapacity = matches
-    //     .map((supervisor) => supervisor.capacity)
-    //     .reduce((prev, current) => prev + current, 0);
-    // console.log(totalCapacity);
-    // // Sort student matches by the priority of the acm keyword matched
-    // matches.forEach((supervisor) => {
-    //     supervisor.matchedStudentResponses.sort((student1, student2) =>
-    //         student1.commonAcmKeyword[0].priority >
-    //         student2.commonAcmKeyword[0].pririty
-    //             ? 1
-    //             : -1
-    //     );
-    //     // almost all of the matches are the same people
-    //     //Take first one since most likely has priortiy of 1
-    //     supervisor.matchedStudentResponses = [
-    //         supervisor.matchedStudentResponses[0],
-    //     ];
-    //     rejectedStudents = [
-    //         ...rejectedStudents,
-    //         ...supervisor.matchedStudentResponses.slice(1),
-    //     ];
-    //     // if (supervisor.takenSpaces > supervisor.capacity) {
-    //     //     // Put student over capacity into leftover array
-    //     //     var remainders = supervisor.matchedStudentResponses.slice(
-    //     //         supervisor.capacity
-    //     //     );
-    //     //     leftOverStudents = [
-    //     //         ...leftOverStudents,
-    //     //         ...remainders.map((student) => student.studentResponse),
-    //     //     ];
-    //     //     // Truncate array of matches based on capacity
-    //     //     supervisor.matchedStudentResponses =
-    //     //         supervisor.matchedStudentResponses.slice(
-    //     //             0,
-    //     //             supervisor.capacity
-    //     //         );
-    //     //     supervisor.takenSpaces = supervisor.capacity;
-    //     // }
-    //     // if (supervisor.takenSpaces < supervisor.capacity) {
-    //     //     var remainingSupervisor = { ...supervisor.supervisorResponse };
-    //     //     remainingSupervisor.capacity = supervisor.capacity -=
-    //     //         supervisor.takenSpaces;
-    //     //     supervisorsWithLeftOverSpace.push(remainingSupervisor);
-    //     // }
-    // });
-    // leftOverStudents = [...new Set(leftOverStudents)];
-    // writeToJson(matches, "json/orderedMatches.json");
-    // return {
-    //     initialMatches: matches,
-    //     leftOverStudents,
-    //     supervisorsWithLeftOverSpace,
-    // };
-};
-
-const collateMatches = (initialMatches, matchesFromRemainders) => {
-    matchesFromRemainders.forEach((match) => {
-        var sameSupervisor = initialMatches.find(
-            (initialMatch) =>
-                initialMatch.supervisorResponse.responseId ===
-                match.supervisorResponse.responseId
-        );
-
-        sameSupervisor.matchedStudentResponses = [
-            ...sameSupervisor.matchedStudentResponses,
-            ...match.matchedStudentResponses,
-        ];
-    });
-
-    return initialMatches;
 };
 
 async function main() {
-    var parsedStudentResponses = await getParsedStudentResponses();
-    var parsedSupervisorResponses = await getParsedSupervisorResponses();
+    const parsedStudentResponses = await getParsedStudentResponses();
+    const parsedSupervisorResponses = await getParsedSupervisorResponses();
 
-    var uniqueStudents = [
+    const numUniqueStudents = [
         ...new Set(
             parsedStudentResponses.map((response) => response.responseId)
         ),
-    ];
+    ].length;
 
-    var uniqueSupervisors = [
+    const numUniqueSupervisors = [
         ...new Set(
             parsedSupervisorResponses.map((response) => response.responseId)
         ),
-    ];
+    ].length;
 
     // Give the first pass amount to each supervisor
     const studentsPerSupervisor = Math.floor(
-        uniqueStudents.length / uniqueSupervisors.length
+        numUniqueStudents / numUniqueSupervisors
     );
 
-    const remainder = uniqueStudents.length % uniqueSupervisors.length;
+    const remainder = numUniqueStudents % numUniqueSupervisors;
 
     // Find all possible matches for each supervisor
     var { matches, unmatchedStudents } = findMatches(
@@ -309,85 +200,17 @@ async function main() {
     matches = orderMatches(matches);
 
     // Write to JSON to check
-    formatAndWriteToJson({ matches }, "json/initialMatches.json");
-    writeToJson({ unmatchedStudents }, "json/unmatchedStudents.json");
+    //formatAndWriteToJson({ matches }, "json/initialMatches.json");
 
     // Review matches and take first n from each supervisor
     // If the student has already been picked, move to second place etc.
-    console.log(`Students per supervisor: ${studentsPerSupervisor}`);
 
     var review = await reviewMatches(matches, studentsPerSupervisor, remainder);
     matches = review.matches;
-    console.log(matches);
     var supervisorsWithNoMatches = review.supervisorsWithNoMatches;
 
     formatAndWriteToJson({ matches }, "json/afterReviewMatches.json");
-    console.log("here");
+    writeToJson({ unmatchedStudents }, "json/unmatchedStudents.json");
     writeToJson({ supervisorsWithNoMatches }, "json/unmatchedSupervisors.json");
-
-    // Unmatched students are matched to supervisors that have space left
-    //Any leftover students are submitted as not found
-
-    console.log(`There are ${uniqueStudents.length} unique students`);
-    console.log(`There are ${uniqueSupervisors.length} unique supervisors`);
-    console.log(
-        unmatchedStudents.length > 0
-            ? "Unmatched students"
-            : "All students matched"
-    );
-
-    if (unmatchedStudents.length > 0) {
-        if (supervisorsWithNoMatches.length > 0) {
-            console.log("More matches can be made");
-        } else {
-            console.log("No supervisors left to match");
-        }
-    }
-
-    /**
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     *
-     */
-
-    // var { initialMatches, leftOverStudents, supervisorsWithLeftOverSpace } =
-    //     await reviewMatches();
-    // // Rerun with remaining unmatched students and supervisors
-    // var { matchesFromRemainders, unmatched } = matchRemainders(
-    //     leftOverStudents,
-    //     supervisorsWithLeftOverSpace
-    // );
-
-    // // Collate old and new matches
-
-    // collateMatches(initialMatches, matchesFromRemainders);
-
-    // writeToJson({ unmatched }, "json/unmatched.json");
-
-    // var finalMatches = await collateMatches(
-    //     initialMatches,
-    //     matchesFromRemainders
-    // );
-
-    // formatAndWriteToJson({ finalMatches }, "json/finalMatches.json");
 }
-
 main();
